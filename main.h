4c278ec876b195ee0481c7925de9a762368b4c4b
Pho <pho.dmc5@gmail.com>
2023-08-23 13:44:14 +0000

    add main header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* FUNTION PROTOTYPES */
+int _printf(const char *format, ...);
+int _writechar(char c);
+void print_string(char *str);
+void print_number(int n);
+
+#endif
